1c1ede15e10d885ad85de8199d171dde540abda4
Paulo Ortolan <paulo.ortolan@gmail.com>
2023-11-07 21:29:33 +0000

    Solving all lints

--- lib/movies_reports.py
+++ lib/movies_reports.py
@@ -1,9 +1,16 @@
 """ Reporter module """
 import emoji
 
-from lib.movies_constants import GROUP_LIST_HEADER, GROUP_LIST_HEADER_SEPARATOR, RECORD_FORMAT, GROUP_HEADER, \
-    SEARCH_RESULT_HEADER
-from lib.movies_utils import convert_to_minutes, convert_to_flag, format_duration
+from lib.movies_constants import (
+    GROUP_LIST_HEADER,
+    GROUP_LIST_HEADER_SEPARATOR,
+    RECORD_FORMAT,
+    GROUP_HEADER,
+    SEARCH_RESULT_HEADER)
+from lib.movies_utils import (
+    convert_to_minutes,
+    convert_to_flag,
+    format_duration)
 
 
 class MovieReporter:
@@ -14,6 +21,18 @@ class MovieReporter:
         print(GROUP_LIST_HEADER)
         print(GROUP_LIST_HEADER_SEPARATOR)
 
+    def _print_record(self, result_item):
+        """ Prints the record """
+        print(RECORD_FORMAT.format(
+            result_item["watched"],
+            result_item["id"],
+            result_item["group_id"],
+            result_item["movie_id"],
+            result_item["movie_name"],
+            result_item["duration"],
+            result_item["imdb_rating"],
+            result_item["size_in_gb"]))
+
     def print_group(self, group_number, group_data):
         """ Print group """
         print(GROUP_HEADER.format(group_number))
@@ -21,9 +40,7 @@ class MovieReporter:
         self._print_headers()
 
         for group_item in group_data:
-            print(RECORD_FORMAT.format(group_item["watched"], group_item["id"], group_item["group_id"],
-                                       group_item["movie_id"], group_item["movie_name"], group_item["duration"],
-                                       group_item["imdb_rating"], group_item["size_in_gb"]))
+            self._print_record(group_item)
 
         self._print_summary(group_data)
 
@@ -34,7 +51,10 @@ class MovieReporter:
         print("All Movies\n")
 
         for group in groups:
-            group_data = [movie_item for movie_item in movie_data if movie_item["group_id"] == group]
+            group_data = \
+                [movie_item
+                 for movie_item in movie_data
+                 if movie_item["group_id"] == group]
             self.print_group(group, group_data)
             print("\n")
 
@@ -47,42 +67,56 @@ class MovieReporter:
         self._print_headers()
 
         for search_result in search_results:
-            print(RECORD_FORMAT.format(search_result["watched"], search_result["id"], search_result["group_id"],
-                                       search_result["movie_id"], search_result["movie_name"],
-                                       search_result["duration"],
-                                       search_result["imdb_rating"], search_result["size_in_gb"]))
+            self._print_record(search_result)
+
+    def _filter_watched(self, status_tuple_array):
+        """ Filter watched movies"""
+        return filter(lambda x: x[0] == 1, status_tuple_array)
+
+    def _filter_unwatched(self, status_tuple_array):
+        """ Filter unwatched movies"""
+        return list(filter(lambda x: x[0] == 0, status_tuple_array))
 
     def _print_summary(self, data):
         """ Prints summary every group end """
         status_tuple_array = list(
-            (convert_to_flag(data_item["watched"]), convert_to_minutes(data_item["duration"]), data_item["size_in_gb"])
+            (
+                convert_to_flag(data_item["watched"]),
+                convert_to_minutes(data_item["duration"]),
+                data_item["size_in_gb"]
+            )
             for data_item in data)
 
         total_duration = format_duration(sum(list(map(lambda x: x[1], status_tuple_array))))
         watched_time = format_duration(
-            sum(list(map(lambda x: x[1], list(filter(lambda x: x[0] == 1, status_tuple_array))))))
+            sum(list(map(lambda x: x[1], self._filter_watched(status_tuple_array)))))
         time_left = format_duration(
-            sum(list(map(lambda x: x[1], list(filter(lambda x: x[0] == 0, status_tuple_array))))))
+            sum(list(map(lambda x: x[1], self._filter_unwatched(status_tuple_array)))))
 
         total_size = sum(list(map(lambda x: x[2], status_tuple_array)))
-        consumed_gb = sum(list(map(lambda x: x[2], list(filter(lambda x: x[0] == 1, status_tuple_array)))))
-        left_gb = sum(list(map(lambda x: x[2], list(filter(lambda x: x[0] == 0, status_tuple_array)))))
+        consumed_gb = (
+            sum(list(map(lambda x: x[2], self._filter_watched(status_tuple_array)))))
+        left_gb = (
+            sum(list(map(lambda x: x[2], self._filter_unwatched(status_tuple_array)))))
 
         watched_movies = len(list(filter(lambda x: x[0] == 1, status_tuple_array)))
         not_watched_movies = len(list(filter(lambda x: x[0] == 0, status_tuple_array)))
         progress = ((watched_movies / len(status_tuple_array)) * 100)
 
         print(f"\n{emoji.emojize(':bar_chart:')} Status\n")
-        print(f"-[ {emoji.emojize(':mantelpiece_clock:')} TIME ]-------------------------------------------------------------------------")
+        print(f"-[ {emoji.emojize(':mantelpiece_clock:')} TIME "
+              f"]-------------------------------------------------------------------------")
         print(f"Total Duration .....: {total_duration}")
         print(f"Watched Time .......: {watched_time}")
         print(f"Time Left ..........: {time_left}")
-        print(f"\n-[ {emoji.emojize(':computer_disk:')} Size ]-------------------------------------------------------------------------")
-        print("Total Size ..........: {:>7.2f}".format(total_size))
-        print("Consumed GB .........: {:>7.2f}".format(consumed_gb))
-        print("Left GB .............: {:>7.2f}".format(left_gb))
-        print(f"\n-[ {emoji.emojize(':movie_camera:')} Watched Movies ]---------------------------------------------------------------")
-        print("Watched .............: {:>7.0f}".format(watched_movies))
-        print("Not Watched .........: {:>7.0f}".format(not_watched_movies))
-        print("Progress ............: {:>7.2f} %".format(progress))
-        print("----------------------------------------------------------------------------------\n")
+        print(f"\n-[ {emoji.emojize(':computer_disk:')} Size "
+              f"]-------------------------------------------------------------------------")
+        print(f"Total Size ..........: {total_size:>7.2f}")
+        print(f"Consumed GB .........: {consumed_gb:>7.2f}")
+        print(f"Left GB .............: {left_gb:>7.2f}")
+        print(f"\n-[ {emoji.emojize(':movie_camera:')} Watched Movies "
+              f"]---------------------------------------------------------------")
+        print(f"Watched .............: {watched_movies:>7.0f}")
+        print(f"Not Watched .........: {not_watched_movies:>7.0f}")
+        print(f"Progress ............: {progress:>7.2f} %")
+        print(f"{'-' * 85}\n")
